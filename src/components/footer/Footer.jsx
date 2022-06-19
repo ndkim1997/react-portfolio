c72c360c5a4eac49ab67cb8d4fee72f11ac9fa4a
ndkim1997 <kim.nick7@gmail.com>
2022-06-18 22:24:47 -0700

    small sytling edits

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <ul className='permalinks'>
         <li><a href='#'>Home</a></li>
         <li><a href='#about'>About</a></li>
-        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#experience'>Skills</a></li>
         <li><a href='#portfolio'>Portfolio</a></li>
         <li><a href='#contact'>Contacts</a></li>
       </ul>
